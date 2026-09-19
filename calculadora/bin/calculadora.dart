2ed80da1487b61d8923f74a487bb364574860883
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-09-19 11:31:20 -0300

    Fazendo a validação

--- calculadora/bin/calculadora.dart
+++ calculadora/bin/calculadora.dart
@@ -4,6 +4,7 @@ void main() {
   double numeroUm = 0;
   double numeroDois = 0;
   String operacao = "";
+  String? entrada = "";
   List<String> operacoes = <String>["+", "-", "*", "/"];
 
   void soma() {
@@ -39,38 +40,36 @@ void main() {
     }
   }
 
-  print("Digite o primeiro valor");
-
-  String? entrada = stdin.readLineSync();
-
-  if (entrada != null) {
-    if (entrada != "") {
-      numeroUm = double.parse(entrada);
-    }
-  }
-
   void getOperacao() {
     print("Digite uma operação");
     entrada = stdin.readLineSync();
 
-    if (entrada != null) {}
+    if (entrada != null) {
+      if (operacoes.contains(entrada)) {
+        operacao = entrada!;
+      }
+    }
   }
 
-  print("Digite uma operação");
+  print("Digite o primeiro valor");
 
-  entrada = stdin.readLineSync();
-
-  if (entrada != null) {
-    operacao = entrada;
-  }
-
-  print("Digite o segundo valor");
-
-  entrada = stdin.readLineSync();
+  entrada = stdin.readLineSync()!;
 
   if (entrada != null) {
     if (entrada != "") {
-      numeroDois = double.parse(entrada);
+      numeroUm = double.parse(entrada!);
+    }
+  }
+
+  getOperacao();
+
+  print("Digite o segundo valor");
+
+  entrada = stdin.readLineSync()!;
+
+  if (entrada != null) {
+    if (entrada != "") {
+      numeroDois = double.parse(entrada!);
     }
   }
 
