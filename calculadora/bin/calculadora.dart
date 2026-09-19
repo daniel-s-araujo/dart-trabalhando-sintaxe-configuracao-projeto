e68395bfb0b247a5ee5c2c486a427b1f58cc0793
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-09-19 11:39:21 -0300

    Refazendo uma operação

--- calculadora/bin/calculadora.dart
+++ calculadora/bin/calculadora.dart
@@ -41,12 +41,15 @@ void main() {
   }
 
   void getOperacao() {
-    print("Digite uma operação");
+    print("Digite uma operação ${operacoes.toString()}");
     entrada = stdin.readLineSync();
 
     if (entrada != null) {
       if (operacoes.contains(entrada)) {
         operacao = entrada!;
+      } else {
+        print("Operação inválida");
+        getOperacao();
       }
     }
   }
