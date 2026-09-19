d50a109b2f44b90041bcff0311b8a77c7c5d471f
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-09-19 11:18:34 -0300

    Lista de operações

--- calculadora/bin/calculadora.dart
+++ calculadora/bin/calculadora.dart
@@ -4,6 +4,7 @@ void main() {
   double numeroUm = 0;
   double numeroDois = 0;
   String operacao = "";
+  List<String> operacoes = <String>["+", "-", "*", "/"];
 
   void soma() {
     print(numeroUm + numeroDois);
@@ -46,6 +47,13 @@ void main() {
     if (entrada != "") {
       numeroUm = double.parse(entrada);
     }
+  }
+
+  void getOperacao() {
+    print("Digite uma operação");
+    entrada = stdin.readLineSync();
+
+    if (entrada != null) {}
   }
 
   print("Digite uma operação");
