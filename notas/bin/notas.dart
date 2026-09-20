faa319d6c178c4317aa9b90725b0dbde5830a385
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-09-20 09:14:52 -0300

    Validando os comandos do programa

--- notas/bin/notas.dart
+++ notas/bin/notas.dart
@@ -1,9 +1,18 @@
+import 'dart:io';
+
 void main() {
-  
+  getComando();  
 }
 
 String getComando() {
   print("Digite um comando: 1 - Adicionar nota, 2 - Listar notas, 3 - Sair");
   List<String> comandos = <String>["1", "2", "3"];
-  String? entrada = "";
+  String? entrada = stdin.readLineSync();
+
+  if (entrada == null || !comandos.contains(entrada)) {
+    print("Comando inválido");
+    getComando();
+  }
+
+  return entrada!;
 }
