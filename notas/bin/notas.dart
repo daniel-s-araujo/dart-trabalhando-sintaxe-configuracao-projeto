732c5f4c248da865a1f7acf4eca7cc44861a5a31
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-09-20 10:11:07 -0300

    Personalizando a aplicação de notas com ASCII art

--- notas/bin/notas.dart
+++ notas/bin/notas.dart
@@ -41,7 +41,11 @@ void listarNotas(List<String> notas) {
 }
 
 void menu(List<String> notas) {
+  print("");
+  cabecalho();
+  print("");
   String comando = getComando();
+  print("");
 
   switch (comando) {
     case "1":
@@ -53,4 +57,12 @@ void menu(List<String> notas) {
     case "3":
       print("Até breve!");
   }
+}
+
+void cabecalho() {
+  print(" █   █  ███  █████  ███   ████ ");
+  print(" ██  █ █   █   █   █   █ █     ");
+  print(" █ █ █ █   █   █   █████  ███  ");
+  print(" █  ██ █   █   █   █   █     █ ");
+  print(" █   █  ███    █   █   █ ████  ");
 }
